ad5d1d06e4e1e8ef9321bb21c32b625f17d2e939
rudra141004 <152824686+rudra141004@users.noreply.github.com>
2024-11-13 21:18:20 -0700

    Update auth-context.js

--- app/_utils/auth-context.js
+++ app/_utils/auth-context.js
@@ -1,60 +1,60 @@
-"use client";
+// "use client";
  
-import { useContext, createContext, useState, useEffect } from "react";
-import {
-  signInWithPopup,
-  signOut,
-  onAuthStateChanged,
-  GithubAuthProvider,
-  GoogleAuthProvider,
-  createUserWithEmailAndPassword,
-  signInWithEmailAndPassword
-} from "firebase/auth";
-import { auth } from "./firebase";
+// import { useContext, createContext, useState, useEffect } from "react";
+// import {
+//   signInWithPopup,
+//   signOut,
+//   onAuthStateChanged,
+//   GithubAuthProvider,
+//   GoogleAuthProvider,
+//   createUserWithEmailAndPassword,
+//   signInWithEmailAndPassword
+// } from "firebase/auth";
+// import { auth } from "./firebase";
  
-const AuthContext = createContext();
+// const AuthContext = createContext();
  
-export const AuthContextProvider = ({ children }) => {
-  const [user, setUser] = useState(null);
+// export const AuthContextProvider = ({ children }) => {
+//   const [user, setUser] = useState(null);
  
-  const gitHubSignIn = () => {
-    const provider = new GithubAuthProvider();
-    return signInWithPopup(auth, provider);
-  };
+//   const gitHubSignIn = () => {
+//     const provider = new GithubAuthProvider();
+//     return signInWithPopup(auth, provider);
+//   };
  
-  const googleSignIn = () => {
-    const provider = new GoogleAuthProvider();
-    return signInWithPopup(auth, provider);
-  };
+//   const googleSignIn = () => {
+//     const provider = new GoogleAuthProvider();
+//     return signInWithPopup(auth, provider);
+//   };
 
-  // Sign up with email and password
-const emailSignUp = (email, password) => {
-    return createUserWithEmailAndPassword(auth, email, password);
-  };
+//   // Sign up with email and password
+// const emailSignUp = (email, password) => {
+//     return createUserWithEmailAndPassword(auth, email, password);
+//   };
   
-  // Sign in with email and password
-  const emailSignIn = (email, password) => {
-    return signInWithEmailAndPassword(auth, email, password);
-  };
+//   // Sign in with email and password
+//   const emailSignIn = (email, password) => {
+//     return signInWithEmailAndPassword(auth, email, password);
+//   };
 
-  const firebaseSignOut = () => {
-    return signOut(auth);
-  };
+//   const firebaseSignOut = () => {
+//     return signOut(auth);
+//   };
  
-  useEffect(() => {
-    const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
-      setUser(currentUser);
-    });
-    return () => unsubscribe();
-  }, [user]);
+//   useEffect(() => {
+//     const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+//       setUser(currentUser);
+//     });
+//     return () => unsubscribe();
+//   }, [user]);
  
-  return (
-    <AuthContext.Provider value={{ user, gitHubSignIn, emailSignUp, emailSignIn, googleSignIn,firebaseSignOut }}>
-      {children}
-    </AuthContext.Provider>
-  );
-};
+//   return (
+//     <AuthContext.Provider value={{ user, gitHubSignIn, emailSignUp, emailSignIn, googleSignIn,firebaseSignOut }}>
+//       {children}
+//     </AuthContext.Provider>
+//   );
+// };
  
-export const useUserAuth = () => {
-  return useContext(AuthContext);
-};
+// export const useUserAuth = () => {
+//   return useContext(AuthContext);
+// };
